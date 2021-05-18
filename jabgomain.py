e90f517ad221a77ceca383b501765034d2ca3be0
Forgot09 <80463183+Forgot09@users.noreply.github.com>
2021-05-18 22:13:03 +0900

    Update jabgomain.py
    add: jgmuipkn

--- jabgomain.py
+++ jabgomain.py
@@ -1,8 +1,16 @@
 list1 =[]#표의 위쪽 대입
 list2 = []#표의 아래쪽 원래
+list3 = []#
 cryptosystemname = 0
 
-def jghjbfn():#잡고 하짐 받름니, 암호 파일 만들기
+def jgshgn():
+	print('글자를 한글자씩 입력해주세요. 끝나셨으면 "end"라고 입력을 해주세요 ex)한글, ㅎ ㅏ ㄴ ㄱ ㅡ ㄹ')
+	inputf = 0
+	while inputf == 'end':
+		inputf = str(input('> '))
+		list3.append(inputf)
+	
+def jghjbrn():#잡고 하짐 받름니, 암호 파일 만들기
     cryptosystemname = cryptosystemname + '.txt'
     f = open(cryptosystemname, 'a')
 
@@ -17,6 +25,8 @@ def jghjbfn():#잡고 하짐 받름니, 암호 파일 만들기
     for i in list2:
         f.write(i, '|')
     f.write('\n')
+#jghjbrn end
+	jgshgn()
 
 def jgmuipkn():#잡고미으프쿠나, 암호 리스트 추가
     cryptosystemname = str(input('만들 암호체계 이름을 저장해주세요.'))
@@ -38,4 +48,4 @@ def jgmuipkn():#잡고미으프쿠나, 암호 리스트 추가
         list2.append(inputdata)
     print("list2 입력이 끝났습니다")
 
-    jghjbfn()
+    jghjbrn()
